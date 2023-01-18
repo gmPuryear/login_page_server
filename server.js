16cc44250bda2a1ddcbd2212f41776bad82b8249
George Puryear <gm.puryear@yahoo.com>
2023-01-18 16:46:48 -0600

    Initial commit with basic server with one get endpoint

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // simple get request and sends data to browser "API RUNNING"
 app.get('/', (req, res) => res.send('API RUNNING'));
 
-// looks for evironment variable called PORT to use when we deploy to heroku. If no environment set it will default to 5000
+// looks for environment variable called PORT to use when we deploy to heroku. If no environment set it will default to 5000
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
